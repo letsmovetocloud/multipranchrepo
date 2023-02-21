dc3614bfe4f8288340e8f744d1982093cb5579f0
letsmovetocloud <88219893+letsmovetocloud@users.noreply.github.com>
2023-02-21 19:57:11 +0530

    azure to parameters changed to gcp

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/azure"
+      source  = "hashicorp/gcp"
       
     }
   }
@@ -11,26 +11,26 @@ terraform {
 # Provider Block
 provider "aws" {
   profile = "default" 
-  region  = "us-central"
+  region  = "ap-south1-1"
 }
 
 #Resource Block
 resource "aws_instance" "ec2testserver" {
   ami           = "ami-041db4a969fe3eb68" 
-  instance_type = "m4.xlarge"
+  instance_type = "gp.instances "
 
 }
 
 
 # Create VPC Terraform Module
 module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
+  source  = "terraform-gcp-modules/vpc/gcp"
   
 
   # VPC Basic Details
   name = "vpc-dev"
   cidr = "10.0.0.0/16"   
-  azs                 = ["us-east-1a"]
+  azs                 = ["ap-south1-1"]
   private_subnets     = ["10.0.1.0/24"]
   public_subnets      = ["10.0.101.0/24"]
 
